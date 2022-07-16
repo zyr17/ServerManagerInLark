77edbdef1540e60c102944e0324f6c00acf3264a
zyr17 <jzjqz17@gmail.com>
2022-07-16 18:34:03 +0800

    update

--- autossh.sh
+++ autossh.sh
@@ -1,1 +1,1 @@
-autossh -i '~/.ssh/id_rsa' -M 40274 -N -R *:29980:127.0.0.1:29980 -o GatewayPorts=true node26@t.zyr17.cn -p 17022
+autossh -i '~/.ssh/id_rsa' -M 40274 -N -R *:29980:127.0.0.1:29980 -o GatewayPorts=true node26@mdm.zyr17.cn -p 30022
